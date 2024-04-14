618b4ec2d6fdb9baba29503daa2f89f802b2cf8d
namnamyeha <166977217+namnamyeha@users.noreply.github.com>
2024-04-14 15:33:04 +0900

    love
    ll

--- nplifi/src/App.jsx
+++ nplifi/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>준수팀 + 간바레</h1>
+      <h1>💜💜💜💜💜💜💜</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
